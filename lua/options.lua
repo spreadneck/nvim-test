715f51ac84d069003943d72992e9cc506a4efb88
Nate McCure <nate@spreadneck.com>
2025-07-22 14:22:27 -0500

    Refactor plugins into separate files

---
+++ lua/options.lua
@@ -0,0 +1,23 @@
+-- [[ Basic Options ]]
+-- See `:help vim.o`
+
+vim.o.number = true
+vim.o.mouse = 'a'
+vim.o.showmode = false
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+
+-- vim: ts=2 sts=2 sw=2 et
