ca1ff1955702206b4a212cdece4bf9a4b6287b72
Nate McCure <nate@spreadneck.com>
2025-07-22 15:37:39 -0500

    Add extra mini modules and move starter

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,4 @@
+-- [[ Configure plugins ]]
+require('lazy').setup(require('plugins'))
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/mini-starter.lua
@@ -0,0 +1,8 @@
+-- mini.starter configuration
+-- https://github.com/echasnovski/mini.nvim
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    require('mini.starter').setup()
+  end,
+}
